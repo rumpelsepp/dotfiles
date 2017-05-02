c79b8c2bd65897f088e2ef2c2bd486c9459f82a9
Stefan Tatschner <rumpelsepp@sevenbyte.org>
2017-05-02 18:26:38 +0200

    Add atom config

---
+++ .atom/init.js
@@ -0,0 +1,22 @@
+// Your init script
+//
+// Atom will evaluate this file each time a new window is opened. It is run
+// after packages are loaded/activated and after the previous editor state
+// has been restored.
+//
+// An example hack to log to the console when each text editor is saved.
+//
+// atom.workspace.observeTextEditors (editor) ->
+//   editor.onDidSave ->
+//     console.log "Saved! #{editor.getPath()}"
+
+// Toggle between light and dark theme.
+atom.commands.add('atom-workspace', 'dot-atom:toggle-theme', () => {
+    var activeThemes = atom.themes.getActiveThemeNames();
+    
+    if (activeThemes[0].indexOf("light") > 0) {
+        atom.config.set("core.themes", ["one-dark-ui", "one-dark-syntax"]);
+    } else {
+        atom.config.set("core.themes", ["one-light-ui", "one-light-syntax"]);
+    }
+});
