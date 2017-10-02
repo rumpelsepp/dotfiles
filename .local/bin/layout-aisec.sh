27a0dfd36692a700eed44c49aa0ab653762b9605
Stefan Tatschner <rumpelsepp@sevenbyte.org>
2017-10-02 11:07:07 +0200

    Remove trash

--- .local/bin/layout-aisec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --off --output DP2-1 --mode 1920x1080 --pos 0x0 --rotate left --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 3000x728 --rotate normal --output DP1 --off --output DP2-1 --mode 1920x1080 --pos 0x0 --rotate left --output DP2-2 --primary --mode 1920x1080 --pos 1080x728 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
-~/.fehbg
-i3 restart
--- .local/bin/mvws.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -z "$1" ]; then
-    i3-msg move workspace to output left
-else
-    i3-msg move workspace to output $1
-fi
