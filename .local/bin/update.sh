8339f08a3bc8a3e5bf95acdf837d728a4ea0a582
Stefan Tatschner <rumpelsepp@sevenbyte.org>
2018-02-09 17:32:37 +0100

    Add two scripts

---
+++ .local/bin/update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update
+sudo apt full-upgrade
+
+#echo "Start update-atom script..."
+#update-atom.sh
+
+if echo $PATH | grep cargo &> /dev/null; then
+    echo "Start cargo update foo..."
+    cargo install-update -a
+fi
+
+#if echo $PATH | grep -i go &> /dev/null; then
+#    echo "Start go get..."
+#    go get -u all
+#fi
+
+#apm upgrade
