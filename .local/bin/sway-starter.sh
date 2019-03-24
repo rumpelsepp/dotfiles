2cb0d32f0e184d0ea58ed220ce07b191179ca15b
Stefan Tatschner <stefan.tatschner@gmail.com>
2019-03-24 14:35:34 +0100

    Update files

--- .local/bin/sway-starter.sh
+++ .local/bin/sway-starter.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-compgen -c | sort -u | fzf --no-extended --print-query | tail -n1 | xargs -r swaymsg -t command exec
+DATADIR="$HOME/.local/share/sway-starter"
+HISTFILE="$DATADIR/history.txt"
+
+if [[ ! -d "$DATADIR" ]]; then
+    mkdir -p "$DATADIR"
+fi
+
+if [[ ! -f "$HISTFILE" ]]; then
+    touch "$HISTFILE"
+fi
+
+compgen -c | sort -u | fzf --history "$HISTFILE" --no-extended --print-query | tail -n1 | xargs -r swaymsg -t command exec
