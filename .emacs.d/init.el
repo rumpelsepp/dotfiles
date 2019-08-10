91d67a22119ad695541333cf6952dd93e6a51ccb
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2019-08-10 18:31:19 +0200

    fuckit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -138,8 +138,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (evil-collection evil-leader evil-commentary use-package go-mode evil))))
+   '(evil-collection evil-leader evil-commentary use-package go-mode evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
